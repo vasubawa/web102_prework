da7b4424fbb453ad5c81b79137f1258cb41b55f5
Dhruv <vasusharma2000@hotmail.com>
2025-04-25 22:28:18 -0400

    Notsure

--- index.js
+++ index.js
@@ -29,21 +29,24 @@ const gamesContainer = document.getElementById("games-container");
 function addGamesToPage(games) {
 
     // loop over each item in the data
-
-
+    for (let i = 0; i < games.length; i++){
         // create a new div element, which will become the game card
-
+        const element = document.createElement("div");)
 
         // add the class game-card to the list
-
+        element.classList.add("game-card")       
 
         // set the inner HTML using a template literal to display some info 
         // about each game
         // TIP: if your images are not displaying, make sure there is space
         // between the end of the src attribute and the end of the tag ("/>")
+        element.innerHTML = `
+            
+        `;
 
 
         // append the game to the games-container
+    }
 
 }
 
