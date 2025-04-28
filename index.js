5995ec97a30afa7808f35c298b1d03c29fe2e35f
Dhruv <vasusharma2000@hotmail.com>
2025-04-27 23:36:00 -0400

    Challenge7

--- index.js
+++ index.js
@@ -177,7 +177,17 @@ const sortedGames =  GAMES_JSON.sort( (item1, item2) => {
 });
 
 // use destructuring and the spread operator to grab the first and second games
+const [firstGame, secondGame, ...rest] = sortedGames;
 
 // create a new element to hold the name of the top pledge game, then append it to the correct element
+const firstGameElement = document.createElement('p');
+firstGameElement.innerHTML = `${firstGame.name} - $${firstGame.pledged.toLocaleString()}`;
+firstGameContainer.appendChild(firstGameElement);
 
 // do the same for the runner up item
+const secondGameElement = document.createElement('p');
+secondGameElement.innerHTML = `${secondGame.name} - $${secondGame.pledged.toLocaleString()}`;
+secondGameContainer.appendChild(secondGameElement);
+
+console.log("First word of most funded game:", firstGame.name.split(' ')[0]);
+console.log("First word of second most funded game:", secondGame.name.split(' ')[0]);
